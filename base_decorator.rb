8c3f947e55e5fd145b8e7c02ff70a0234ea28107
richemonday@gmail.com <richemonday@gmail.com>
2022-08-10 23:00:57 +0100

    decorate a class

--- base_decorator.rb
+++ base_decorator.rb
@@ -1,12 +1,12 @@
 require './nameable'
 
 class Decorator < Nameable
-    def initialize(nameable)
-        @nameable = nameable
-        super()
-    end
+  def initialize(nameable)
+    @nameable = nameable
+    super()
+  end
 
-     def correct_name
-        @nameable.correct_name
-     end
-    end
+  def correct_name
+    @nameable.correct_name
+  end
+end
--- trimmer_decorator.rb
+++ trimmer_decorator.rb
@@ -1,7 +1,7 @@
 require './base_decorator'
 
-class TrimmerDecorator < Decorator 
-def correct_name
+class TrimmerDecorator < Decorator
+  def correct_name
     @nameable.correct_name.substring!(0, 10)
-end
+  end
 end

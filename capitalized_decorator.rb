d5baf65f53c3771daa2b2acc94894d1e6e48916a
richemonday@gmail.com <richemonday@gmail.com>
2022-08-11 08:48:51 +0100

    added capitalized file"

--- capitalized_decorator.rb
+++ capitalized_decorator.rb
@@ -1,7 +1,7 @@
 require './base_decorator'
 
 class CapitalizedDecorator
-    def correct_name
-        @nameable.correct_name.capitalize
-    end
+  def correct_name
+    @nameable.correct_name.capitalize
+  end
 end
